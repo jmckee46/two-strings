4ea53eea2c760f8da3e748286f32c2bafe52421b
John McKee <jmckee3@mac.com>
2019-11-25 19:01:41 -0800

    working version

--- spec-two-strings_test.go
+++ spec-two-strings_test.go
@@ -3,5 +3,21 @@ package main
 import "testing"
 
 func TestTwoStrings1(t *testing.T) {
+	s1 := "hello"
+	s2 := "world"
+	present := twoStrings(s1, s2)
 
+	if present != "YES" {
+		t.Errorf("got %s instead of YES", present)
+	}
+}
+
+func TestTwoStrings2(t *testing.T) {
+	s1 := "hi"
+	s2 := "world"
+	present := twoStrings(s1, s2)
+
+	if present != "NO" {
+		t.Errorf("got %s instead of NO", present)
+	}
 }
--- two-strings.go
+++ two-strings.go
@@ -1,3 +1,15 @@
 package main
 
+import "strings"
+
+func twoStrings(s1 string, s2 string) string {
+	present := strings.ContainsAny(s1, s2)
+
+	if present {
+		return "YES"
+	}
+
+	return "NO"
+}
+
 func main() {}
